d43995bab99173672e9bc1efa07cd6e79ac49dab
Rik079 <rik@rik079.xyz>
2020-10-16 00:32:22 +0200

    Add placeholder dadjoke search function

--- modules/dadjokes.py
+++ modules/dadjokes.py
@@ -9,11 +9,20 @@ class DadJokes(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def dadjoke(self, ctx):
-        joke = dadjokes.Dadjoke()
-        embed = discord.Embed(title=joke.joke,
-                              color=discord.Color.red())
-        await ctx.send(embed=embed)
+    async def dadjoke(self, ctx, search=''):
+        if search != '':
+            #TODO put the actual search function here
+            joke = dadjokes.Dadjoke()
+            embed = discord.Embed(title=joke.joke,
+                                  colour=discord.Color.blue())
+            embed.set_footer(text="Unfortunately, the dad joke search function "
+                                  "doesn't work yet :( here's a random joke for now!")
+            await ctx.send(embed=embed)
+        else:
+            joke = dadjokes.Dadjoke()
+            embed = discord.Embed(title=joke.joke,
+                                  colour=discord.Color.red())
+            await ctx.send(embed=embed)
 
 
 def setup(bot):
