091ea93a34667ff01a1994e2a2701b1f3ebeab7a
Rik079 <rik@rik079.xyz>
2020-10-15 14:50:40 +0200

    Add Falcon command at Zibs request :P

--- speasier.py
+++ speasier.py
@@ -12,13 +12,17 @@ version = config.__version__
 
 description = """Speasier Discord server text-to-speech bot"""
 
-bot = commands.Bot(command_prefix="!", description=description)
+bot = commands.Bot(command_prefix="?", description=description)
 
 client = discord.Client()
 
 @bot.event
 async def on_ready():
     print(f"The bot is logged in as {bot.user.name} now! have a wonderful day!")
+
+@bot.command()
+async def falconlaunch(ctx):
+    await ctx.send("https://www.youtube.com/watch?v=sB_nEtZxPog")
 
 
 bot.run(token)
