4aae5e3ceaf9b0529d1a12f46d6af9037d5d98de
Stephan Schumacher aka Worthy Alpaca <stephanxxschumacher@gmail.com>
2020-10-18 17:59:12 +0200

    added command to register a voice

--- modules/user.py
+++ modules/user.py
@@ -1,11 +1,35 @@
 # Copyright(C) 2020 Speasy
 
 from discord.ext import commands
+import sqlite3
 
+database = sqlite3.connect("speasier_db.db")
 
 class UserManagement(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command()
+    async def register(self, ctx, cmd):
+        cur = database.cursor()
+        try:
+            cur.execute("SELECT * FROM users WHERE DiscordID = ?", [ctx.author.id])
+            rows = cur.fetchall()
+            database.commit()
+        except sqlite3.Error as er:
+            return await ctx.send(f"An error coccured: {er}")
+        
+        if len(rows) == 1:
+            data = rows[0]
+            return await ctx.send(f"You already registered. `Your voice is {data[2]}`")
+
+        try:
+            cur.execute("INSERT INTO users(DiscordID, Name, Voice) VALUES(?, ?, ?)", [ctx.author.id, ctx.author.name, cmd])
+            database.commit()
+        except sqlite3.Error as er:
+            return await ctx.send(f"An error coccured: {er}")
+
+        await ctx.send("Successfully registered")
 
 
 def setup(bot):
