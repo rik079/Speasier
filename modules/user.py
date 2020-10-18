9b7fd78f9283aec084b068e0097c1247a9cff361
Rik079 <rikoverveld@gmail.com>
2020-10-18 18:06:34 +0200

    Add ?profile command

--- modules/user.py
+++ modules/user.py
@@ -1,11 +1,25 @@
 # Copyright(C) 2020 Speasy
 
+import discord
 from discord.ext import commands
+from modules import database
+
+db = database.database
 
 
 class UserManagement(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command()
+    async def profile(self, ctx):
+        cur = db.cursor()
+        res = cur.execute(f"select Name, voice from users where DiscordID = '{ctx.author.id}'").fetchall()
+        username = res[0][0]
+        embed = discord.Embed(title=username)
+        embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+        embed.add_field(name="Voice:", value=res[0][1])
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
