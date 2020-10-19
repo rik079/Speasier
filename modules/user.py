c36f98c97ade49459fd955b72797904ae02f23d1
Stephan Schumacher aka Worthy Alpaca <stephanxxschumacher@gmail.com>
2020-10-20 00:20:48 +0200

    added role adding and removal

--- modules/user.py
+++ modules/user.py
@@ -2,6 +2,7 @@
 
 import discord
 from discord.ext import commands
+from discord.utils import get
 from modules import database
 import sqlite3
 
@@ -32,7 +33,7 @@ class UserManagement(commands.Cog):
         embed.add_field(name="Voice:", value=rows[0][2])
         await ctx.send(embed=embed)
 
-    @commands.command()
+    @commands.command(pass_context=True)
     async def register(self, ctx, voice):
         cur = database.cursor()
         try:
@@ -47,13 +48,26 @@ class UserManagement(commands.Cog):
             data = rows[0]
             return await ctx.send(f"You already registered. `Your voice is {data[2]}`")
 
+        roleVer = voice.lower() # role to add
+        user = ctx.message.author  # user
+        role = roleVer  # change the name from roleVer to role
+        
+        try:
+            # add the role
+            await user.add_roles(discord.utils.get(user.guild.roles, name=role))
+        except:
+            # if error
+            return await ctx.send('There was an error trying to add the role. Please make sure that the role exists.')
+
         try:
             cur.execute("INSERT INTO users(DiscordID, Name, Voice) VALUES(?, ?, ?)", [
-                        ctx.author.id, ctx.author.name, voice])
+                        ctx.author.id, ctx.author.name, voice.lower()])
             database.commit()
         except sqlite3.Error as er:
             return await ctx.send(f"An error coccured: `{er}`")
 
+        
+        
         await ctx.send("Successfully registered")
 
     @commands.command()
@@ -69,6 +83,17 @@ class UserManagement(commands.Cog):
 
         if len(rows) != 1:
             return await ctx.send("You don't have a registered voice!")
+
+        data = rows[0]
+        roleVer = data[2]  # role to add
+        user = ctx.message.author  # user
+        role = roleVer  # change the name from roleVer to role
+
+        try:
+            # add the role
+            await user.remove_roles(discord.utils.get(user.guild.roles, name=role))
+        except:
+            return await ctx.send('There was an error trying to remove the role. Please make sure that the role exists and is completly lowercase.')
 
         try:
             cur.execute("DELETE FROM users WHERE DiscordID = ?",
