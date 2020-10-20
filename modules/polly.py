4e40aed87dc88404010d195bac8df607da9cbcf5
Rik079 <rikoverveld@gmail.com>
2020-10-21 00:26:17 +0200

    Create Polly cog

---
+++ modules/polly.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2020, Rik079, Worthy Alpaca, Zibadian, Micro-T. All rights reserved.
+
+import speasier
+from discord.ext import commands
+
+polly_client = speasier.polly_client
+
+
+def synth(voice, text, filename='speech'):
+    response = polly_client.synthesize_speech(VoiceId=voice,
+                                              OutputFormat='mp3',
+                                              Text=text)
+    file = open(f'{filename}.mp3', 'wb')
+    file.write(response['AudioStream'].read())
+    file.close()
+
+
+class Polly(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+def setup(bot):
+    bot.add_cog(Polly(bot))
