94f2ba8038b401d9b7305af540f5da701c6ce426
Stephan Schumacher aka Worthy Alpaca <stephanxxschumacher@gmail.com>
2020-10-20 23:31:02 +0200

    added role mentioning as input

--- modules/user.py
+++ modules/user.py
@@ -27,15 +27,20 @@ class UserManagement(commands.Cog):
         if len(rows) != 1:
             return await ctx.send("You don't have a registered voice!")        
         
+        user = ctx.message.author
         username = rows[0][1]
         embed = discord.Embed(title=username)
         embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-        embed.add_field(name="Voice:", value=rows[0][2])
+        embed.add_field(name="Voice:", value=discord.utils.get(user.guild.roles, id=int(rows[0][2])))
         await ctx.send(embed=embed)
 
     @commands.command(pass_context=True)
-    async def register(self, ctx, voice):
+    async def register(self, ctx, voice: discord.Role=None):
+        if not voice:
+            return await ctx.send("You need to mention the voicerole you want to register")
+
         cur = database.cursor()
+        user = ctx.message.author
         try:
             cur.execute("SELECT * FROM users WHERE DiscordID = ?",
                         [ctx.author.id])
@@ -46,22 +51,22 @@ class UserManagement(commands.Cog):
 
         if len(rows) == 1:
             data = rows[0]
-            return await ctx.send(f"You already registered. `Your voice is {data[2]}`")
-
-        roleVer = voice.lower() # role to add
-        user = ctx.message.author  # user
-        role = roleVer  # change the name from roleVer to role
+            return await ctx.send(f"You already registered. `Your voice is {discord.utils.get(user.guild.roles, id=int(data[2]))}`")
+        
+        
+        if voice == None:
+            return await ctx.send("This role doesn't exist. Make sure it's spelled correctly.")
         
         try:
             # add the role
-            await user.add_roles(discord.utils.get(user.guild.roles, name=role))
+            await user.add_roles(voice)
         except:
             # if error
             return await ctx.send('There was an error trying to add the role. Please make sure that the role exists.')
 
         try:
-            cur.execute("INSERT INTO users(DiscordID, Name, Voice) VALUES(?, ?, ?)", [
-                        ctx.author.id, ctx.author.name, voice.lower()])
+            cur.execute("INSERT INTO users(DiscordID, Name, VoiceID) VALUES(?, ?, ?)", [
+                        ctx.author.id, ctx.author.name, voice.id])
             database.commit()
         except sqlite3.Error as er:
             return await ctx.send(f"An error coccured: `{er}`")
@@ -85,13 +90,11 @@ class UserManagement(commands.Cog):
             return await ctx.send("You don't have a registered voice!")
 
         data = rows[0]
-        roleVer = data[2]  # role to add
         user = ctx.message.author  # user
-        role = roleVer  # change the name from roleVer to role
 
         try:
             # add the role
-            await user.remove_roles(discord.utils.get(user.guild.roles, name=role))
+            await user.remove_roles(discord.utils.get(user.guild.roles, id=int(data[2])))
         except:
             return await ctx.send('There was an error trying to remove the role. Please make sure that the role exists and is completly lowercase.')
 
