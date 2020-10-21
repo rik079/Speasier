758275bf85457a9ab1b377d1a533f8b3f251aa69
Stephan Schumacher aka Worthy Alpaca <stephanxxschumacher@gmail.com>
2020-10-21 15:01:07 +0200

    added leave command

--- modules/voice.py
+++ modules/voice.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from speasier import client
 
 class Voice(commands.Cog):
     def __init__(self, bot):
@@ -7,9 +8,16 @@ class Voice(commands.Cog):
     @commands.command()
     async def join(self, ctx):
         # Gets voice channel of message author
+        if ctx.author.voice == None:
+            return await ctx.send("You need to join the voice channel before you can do that")
         channel = ctx.author.voice.channel
         await channel.connect()
-        
+
+    @commands.command()
+    async def leave(self, ctx):
+        if ctx.voice_client == None:
+            return await ctx.send("You need to join the voice channel before you can do that")
+        await ctx.voice_client.disconnect()
 
 def setup(bot):
     bot.add_cog(Voice(bot))
